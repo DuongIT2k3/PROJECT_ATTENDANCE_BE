570c1c6e9c1f9e34aac1deb689c5a0e4b05e53b7
DuongIT2k3 <doanduong161@gmail.com>
2025-08-29 14:53:00 +0700

    hotfix route user

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -2,7 +2,7 @@ import Router from "express";
 import { restrictTo, verifyUser } from "../../common/middlewares/auth.middlewares.js";
 import validBodyRequest from "../../common/middlewares/valid-body.middlewares.js";
 import * as userController from "./user.controller.js";
-import { blockUserSchema, createUserSchema, updateProfileSchema } from "./user.schema.js";
+import { blockUserSchema, createUserSchema, updateProfileSchema, updateRoleSchema } from "./user.schema.js";
 
 const userRoutes = Router();
 
@@ -13,7 +13,7 @@ userRoutes.use(restrictTo(["superAdmin"]));
 userRoutes.post("/", validBodyRequest(createUserSchema), userController.createUser);
 userRoutes.get("/:userId", userController.getUser);
 userRoutes.get("/", userController.getAllUsersController);
-userRoutes.patch("/role/:userId", validBodyRequest(updateProfileSchema), userController.updateRole);
+userRoutes.patch("/role/:userId", validBodyRequest(updateRoleSchema), userController.updateRole);
 userRoutes.patch("/block/:userId", validBodyRequest(blockUserSchema), userController.blockUserController);
 
 
